a53c817519a54af9bf7aaaddc03f6a641de43bd8
kunita <kunita@waltsinc.jp>
2019-10-02 14:42:57 +0900

    update

--- app/main.php
+++ app/main.php
@@ -6,10 +6,13 @@ require_once 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Nesk\Puphpeteer\Puppeteer;
+use Nesk\Rialto\Data\JsFunction;
 use SimpleLog\Logger;
 
 $dotenv = Dotenv::create(__DIR__);
 $dotenv->load();
+
+$logger = new Logger('/dev/stdout', 'default');
 
 $puppeteer = new Puppeteer([
 ]);
@@ -18,7 +21,7 @@ $browser   = $puppeteer->launch([
     'defaultViewport' => [
         'width'            => 800,
         'height'           => 800,
-        'logger'           => new Logger('/dev/stdout', 'default'),
+        'logger'           => $logger,
         'log_node_console' => true,
         'debug'            => true,
     ],
@@ -27,9 +30,25 @@ $browser   = $puppeteer->launch([
     ],
 ]);
 
+$logger->info('ログイン開始');
 $page = $browser->newPage();
 $page->goto('https://ankiweb.net/account/login');
-
 $page->type('#email', getenv('ANKI_WEB_ID'));
 $page->type('#password', getenv('ANKI_WEB_PW'));
-$page->screenshot(['path' => '/app/test.png']);
+$page->querySelector('input[type="submit"]')->press('Enter');
+$page->waitForNavigation();
+//$page->screenshot(['path' => '/app/test.png']);
+$logger->info('ログイン終了');
+
+$logger->info('カード追加開始');
+$page->goto('https://ankiuser.net/edit/');
+$page->querySelectorEval('#deck', JsFunction::createWithParameters(['node'])
+    ->body('node.value = ""'));
+$page->type('#deck', '000_test');
+$page->type('#f0', 'test');
+$page->type('#f1', 'test');
+$page->querySelector('button.btn-primary')->press('Enter');
+//$page->screenshot(['path' => '/app/test.png']);
+$logger->info('カード追加完了');
+
+$browser->close();
